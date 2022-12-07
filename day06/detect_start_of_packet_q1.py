29295d48856dfa62b194e54abcff40be3635cbf8
Jean-Yves Tinevez <tinevez@pasteur.fr>
2022-12-07 19:59:05 +0100

    Solution to day 6, with a nice circular buffer.

---
+++ day06/detect_start_of_packet_q1.py
@@ -0,0 +1,20 @@
+import collections
+
+msg_size =4
+
+buffer = collections.deque(maxlen=msg_size)
+i = 0
+with open('input.txt', 'r') as f:
+    while True:
+         
+        # Read from file
+        c = f.read(1)
+        if not c:
+            break
+
+        i += 1
+        buffer.append(c)
+        if len(buffer) >= msg_size:
+            if len(set(buffer)) == len(buffer):
+                print('First start-of-packet marker: %d' % i)
+                break
